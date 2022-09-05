5bc82cdcfeb05865a3c67f20392b8228caefc997
= <=>
2022-09-05 11:34:45 -0300

    first commit

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from core.models import Categoria, Marca, Carro
+
+admin.site.register(Categoria)
+admin.site.register(Marca) 
+admin.site.register(Carro)
+# Register your models here.
