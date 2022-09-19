f0dfdeb7759bdbd45d6ccee3b2e864333addd4ee
Amanda Vincenzi <amandavincenzi.ifc@gmail.com>
2022-09-19 12:14:45 -0300

    commit garagemdnv

--- garagem/urls.py
+++ garagem/urls.py
@@ -1,5 +1,4 @@
 """garagem URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
@@ -14,8 +13,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from core.views import CategoriaViewSet, MarcaViewSet, CarroViewSet
+
+router = DefaultRouter()
+router.register(r'categorias', CategoriaViewSet)
+router.register(r'marcas', MarcaViewSet)
+router.register(r'carros', CarroViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
